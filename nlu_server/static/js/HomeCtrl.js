2f591ffeef0361cfc61f1e9d28a450ed931ed952
charles <charlsehsu@mercue.biz>
2018-04-09 10:25:35 +0800

    update

--- nlu_server/static/js/HomeCtrl.js
+++
@@ -1,9 +0,0 @@
-
-var appControllers = angular.module('app.homectrl', []);
-
-appControllers.controller('homeCtrl', 
-		['$scope', '$http', 'MercueRequests',
-		 function ($scope, $http, MercueRequests){
-			
-		console.log("home ctrl...");
-}]);
