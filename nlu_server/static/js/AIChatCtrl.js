24d52e642ed35743a95044a04eafda75837e244d
q27913588@gmail.com <q27904488>
2018-04-27 12:32:07 +0800

    update chat

--- nlu_server/static/js/AIChatCtrl.js
+++ nlu_server/static/js/AIChatCtrl.js
@@ -5,7 +5,22 @@ appControllers.controller('AIChatCtrl',
 		['$scope', '$http', 'MercueRequests', '$state','ModalService',
 		 function ($scope, $http, MercueRequests,$state,ModalService){
 
-           
+		   $scope.messageList = [];
+		   setData();
+		   function setData(){
+			   var msg = {}
+			   msg.text = "測試"
+			   msg.sender = "admin"
+			   $scope.messageList.push(msg);
+			   $scope.messageList.push(msg);
+			   var msg = {}
+			   msg.text = "測試"
+			   msg.sender = "cutomer"
+			   $scope.messageList.push(msg);
+			   $scope.messageList.push(msg);
+			   console.log("sets");
+			   console.log( $scope.messageList);
+		   }
             
 			$scope.editIntent = function(){
 				console.log("edit")
@@ -29,6 +44,37 @@ appControllers.controller('AIChatCtrl',
             
 			$scope.submit = function(){
 				 console.log("submit")
+				 var msg = {}
+				 msg.text = $scope.textInput;
+				 msg.sender = "cutomer"
+				 $scope.messageList.push(msg);
+				 $scope.textInput = "";
+			}
+
+			$scope.checkBtn = function(message){
+				if(message.sender == "admin"){
+					return false
+				}else{
+					return true
+				}
+
+			}
+			$scope.checkAdmin = function(message){
+				console.log(message);
+				if(message.sender == "admin"){
+					return "angent-text"
+				}else{
+					return "custom-text"
+				}
+				
+			}
+			$scope.checkTextRight = function(message){
+				if(message.sender == "admin"){
+					return "border-lp"
+				}else{
+					return "border-rp"
+				}
+				
 			}
 		console.log("chat ctrl...");
 }]);
