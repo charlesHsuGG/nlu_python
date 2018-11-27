65ae49f8c7ced50be9043ef2d866731e4bacd35d
q27913588@gmail.com <q27904488>
2018-11-27 10:49:56 +0800

    updatae
    update

--- nlu_server/webapp/static/js/AIEntityListCtrl.js
+++ nlu_server/webapp/static/js/AIEntityListCtrl.js
@@ -1,25 +1,27 @@
-
 var appControllers = angular.module('app.aientitylistctrl', []);
 
 appControllers.controller('AIEntityListCtrl',
-	['$scope', '$http', 'MercueRequests', '$state','ModalService',
-		function ($scope, $http, MercueRequests, $state,ModalService) {
+	['$scope', '$http', 'MercueRequests', '$state', 'ModalService',
+		function ($scope, $http, MercueRequests, $state, ModalService) {
 
 			console.log("list ctrl...");
 			$scope.admin_id = "40w9dse0277455f634fw40439sd";
-    		$scope.model_id = "024a140e177851ea83a36ef0ed9b1ddd";
+			$scope.model_id = "024a140e177851ea83a36ef0ed9b1ddd";
 			init();
+
 			function init() {
 				loadData();
 			}
- 
+
 
 			$scope.train = function () {
 				$http({
 					method: 'POST',
 					url: './train',
-					data: { "admin_id": $scope.admin_id,
-							"model_id":$scope.model_id  }
+					data: {
+						"admin_id": $scope.admin_id,
+						"model_id": $scope.model_id
+					}
 				}).then(function successCallback(response) {
 					console.log(response);
 					alert("訓練完成");
@@ -32,48 +34,56 @@ appControllers.controller('AIEntityListCtrl',
 				$http({
 					method: 'POST',
 					url: './ai_entity/entity_list',
-					data: { "admin_id": $scope.admin_id,
-							"model_id": $scope.model_id}
+					data: {
+						"admin_id": $scope.admin_id,
+						"model_id": $scope.model_id
+					}
 				}).then(function successCallback(response) {
 					console.log(response);
 					var editData = response.data;
-
+				 
 					$scope.entity_list = editData.entities;
 
 				}, function errorCallback(response) {
-					
+
 				});
 
 			}
 
-			$scope.addSlots = function(){
+			$scope.addSlots = function () {
 				console.log("add")
 				ModalService.showModal({
 					templateUrl: "/ai/static/views/aislotadd_modal.html",
 					controller: "aiSlotAddModalCtrl",
 					inputs: {
-						data:  {}
-					  },
-					preClose: (modal) => { modal.element.modal('hide'); } 
-				}).then(function(modal) {
-					modal.element.on('hidden.bs.modal', function () {$('.ngmodal').remove(); });
+						data: {}
+					},
+					preClose: (modal) => {
+						modal.element.modal('hide');
+					}
+				}).then(function (modal) {
+					modal.element.on('hidden.bs.modal', function () {
+						$('.ngmodal').remove();
+					});
 					modal.element.modal();
-					modal.close.then(function(data) {
+					modal.close.then(function (data) {
 						console.log(data);
-						if(data != "cancel"){
+						if (data != "cancel") {
 							addSlotRequest(data);
 						}
 					});
 				});
 			}
-			$scope.editIntent = function(index){
+			$scope.editIntent = function (index) {
 				console.log(index);
 				console.log($scope.entity_list[index].entity_id);
 				var id = $scope.entity_list[index].entity_id;
 				$http({
 					method: 'POST',
 					url: './ai_entity/entity_get',
-					data: {"entity_id":id}
+					data: {
+						"entity_id": id
+					}
 				}).then(function successCallback(response) {
 					console.log(response);
 					var resData = response.data;
@@ -82,34 +92,59 @@ appControllers.controller('AIEntityListCtrl',
 						templateUrl: "/ai/static/views/aislotadd_modal.html",
 						controller: "aiSlotAddModalCtrl",
 						inputs: {
-							data:  resData
-						  },
-						preClose: (modal) => { modal.element.modal('hide'); } 
-					}).then(function(modal) {
-						modal.element.on('hidden.bs.modal', function () {$('.ngmodal').remove(); });
+							data: resData
+						},
+						preClose: (modal) => {
+							modal.element.modal('hide');
+						}
+					}).then(function (modal) {
+						modal.element.on('hidden.bs.modal', function () {
+							$('.ngmodal').remove();
+						});
 						modal.element.modal();
-						modal.close.then(function(data) {
+						modal.close.then(function (data) {
 							console.log(data);
-							if(data != "cancel"){
+							if (data != "cancel") {
 								// $scope.slotsList.push(data);
 								console.log(data);
-	
+
 							}
 						});
 					});
 
 
 				}, function errorCallback(response) {
-					
+
 				});
 
-				
-				
-			 
+
+
+
 			}
-	
+			//刪除	
+			$scope.deleteIntent  = function (index) {
+				console.log(index);
+				console.log($scope.entity_list[index].entity_id);
+				var id = $scope.entity_list[index].entity_id;
+				$http({
+					method: 'POST',
+					url: './ai_entity/entity_delete',
+					data: {
+						"entity_id": id
+					}
+				}).then(function successCallback(response) {
+					console.log(response);
+					var resData = response.data;
+					console.log(resData);
+					loadData();
+
+				}, function errorCallback(response) {
+
+				});
+
+			}
 			function addSlotRequest(data) {
-				
+
 				var finalObj = {};
 				var sendArray = [];
 				var sendObj = {};
@@ -117,7 +152,7 @@ appControllers.controller('AIEntityListCtrl',
 				sendObj.entity_value_list = data.entity_value;
 				sendArray.push(sendObj);
 				finalObj.admin_id = "40w9dse0277455f634fw40439sd";
-				finalObj.entities =  sendArray;
+				finalObj.entities = sendArray;
 				console.log(finalObj);
 				$http({
 					method: 'POST',
@@ -128,8 +163,9 @@ appControllers.controller('AIEntityListCtrl',
 					loadData();
 
 				}, function errorCallback(response) {
-					
+
 				});
 
 			}
-		}]);
+		}
+	]);
--- nlu_server/webapp/static/js/AISlotAddModal.js
+++ nlu_server/webapp/static/js/AISlotAddModal.js
@@ -1,8 +1,8 @@
 'use strict';
 var appControllers = angular.module('app.aislotaddmodalctrl', []);
 
-appControllers.controller('aiSlotAddModalCtrl',['$scope', '$http','$window' ,'close','MercueRequests','DTOptionsBuilder','data' ,function ($scope,$http,$window,close,MercueRequests,DTOptionsBuilder,data){
-	 
+appControllers.controller('aiSlotAddModalCtrl', ['$scope', '$http', '$window', 'close', 'MercueRequests', 'DTOptionsBuilder', 'data', function ($scope, $http, $window, close, MercueRequests, DTOptionsBuilder, data) {
+
 	console.log("call slotCtrlctrl...");
 	$scope.promptList = [];
 	$scope.sendData = {};
@@ -10,16 +10,17 @@ appControllers.controller('aiSlotAddModalCtrl',['$scope', '$http','$window' ,'cl
 	init();
 	$scope.admin_id = "40w9dse0277455f634fw40439sd";
 	$scope.model_id = "024a140e177851ea83a36ef0ed9b1ddd";
-	
-	$scope.sendData.entity_value = "" ;
-	function init()    {
+
+	$scope.sendData.entity_value = "";
+
+	function init() {
 		console.log(data);
 		var dataPost = {};
 		dataPost.admin_id = "40w9dse0277455f634fw40439sd";
-		if (data.entity_name != null){
-			console.log( data.entity_value_list);
+		if (data.entity_name != null) {
+			console.log(data.entity_value_list);
 			$scope.sendData.entity = data.entity_name;
-			if( data.entity_value_list != null){
+			if (data.entity_value_list != null) {
 				$scope.promptList = data.entity_value_list;
 			}
 
@@ -38,13 +39,13 @@ appControllers.controller('aiSlotAddModalCtrl',['$scope', '$http','$window' ,'cl
 		// 	console.log(response);
 		// });
 
-		setTimeout(function(){
-		 
-		},250);
-		
+		setTimeout(function () {
+
+		}, 250);
+
 	}
-	$scope.addPrompt = function(){
-		if($scope.sendData.entity_value == ""){
+	$scope.addPrompt = function () {
+		if ($scope.sendData.entity_value == "") {
 			alert("請輸入值");
 			return
 		}
@@ -55,29 +56,30 @@ appControllers.controller('aiSlotAddModalCtrl',['$scope', '$http','$window' ,'cl
 		$scope.sendData.entity_value = "";
 		console.log($scope.promptList);
 	}
-	$scope.deletePrompt = function(index){
-	 $scope.promptList.splice(index,1);
+	$scope.deletePrompt = function (index) {
+		$scope.promptList.splice(index, 1);
+		
 	}
-	$scope.close = function(result) {
-		 console.log($scope.promptList.length);
-		if($scope.promptList.length == 0){
+	$scope.close = function (result) {
+		console.log($scope.promptList.length);
+		if ($scope.promptList.length == 0) {
 			alert("請新增值");
 			return;
-		}else if($scope.sendData.entity  == "") {
+		} else if ($scope.sendData.entity == "") {
 			{
 				alert("請填入類別");
-			return;
+				return;
 			}
 		}
 
 		$scope.sendData.entity_value = $scope.promptList;
-		console.log( $scope.sendData);
-			if(result == "cancel"){
-				close("cancel", 500);
-			}else{
-				close($scope.sendData, 500);
-			}
-			
+		console.log($scope.sendData);
+		if (result == "cancel") {
+			close("cancel", 500);
+		} else {
+			close($scope.sendData, 500);
+		}
+
 
 	};
 
